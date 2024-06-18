3dcd2904663ad4488caa051981d5097d6ffbf237
Aige <aigestudio@hotmail.com>
2024-06-18 19:14:42 +0800

    初始化

---
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TheUltimate70HoursIOSDevelopmentBootcamp
+//
+//  Created by Aige on 2024/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
