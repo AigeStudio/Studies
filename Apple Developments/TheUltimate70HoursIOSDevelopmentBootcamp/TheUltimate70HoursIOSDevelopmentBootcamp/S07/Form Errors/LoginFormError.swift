63a1a4067a9b1fc14bd8cd92520256e6962588c7
Aige <aigestudio@hotmail.com>
2024-06-28 22:17:17 +0800

    第七课示例二

---
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/S07/Form Errors/LoginFormError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginFormError.swift
+//  TheUltimate70HoursIOSDevelopmentBootcamp
+//
+//  Created by AigeStudio on 28/6/2024.
+//
+
+import Foundation
+
+struct LoginFormError {
+    var email: String = ""
+    var password: String = ""
+}
--- Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/S07/S07LoginView.swift
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/S07/S07LoginView.swift
@@ -10,16 +10,41 @@ import SwiftUI
 struct S07LoginView: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var loginFormError = LoginFormError()
+    private func clearForm() {
+        loginFormError = LoginFormError()
+    }
+
     var isFormValid: Bool {
-        return !email.isEmpty && !password.isEmpty && email.isValidEmail
+        clearForm()
+
+        if email.isEmpty {
+            loginFormError.email = "Email is required!"
+        } else if !email.isValidEmail {
+            loginFormError.email = "Email is not in correct format!"
+        }
+        if password.isEmpty {
+            loginFormError.password = "Password is required!"
+        }
+        return loginFormError.email.isEmpty && loginFormError.password.isEmpty
     }
 
     var body: some View {
         Form {
             TextField("Email", text: $email)
                 .textInputAutocapitalization(.never)
+            if !loginFormError.email.isEmpty {
+                Text(loginFormError.email)
+                    .font(.caption)
+            }
             SecureField("Password", text: $password)
-            Button("Login") {}.disabled(!isFormValid)
+            if !loginFormError.password.isEmpty {
+                Text(loginFormError.password)
+                    .font(.caption)
+            }
+            Button("Login") {
+                if isFormValid {}
+            }
         }
     }
 }
