a2f866e7bf865f402b4ef309d7feeed714b44f3e
Aige <aigestudio@hotmail.com>
2024-07-05 09:09:32 +0800

    初始化 Budgets 项目

---
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/BudgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheUltimate70HoursIOSDevelopmentBootcampApp.swift
+//  TheUltimate70HoursIOSDevelopmentBootcamp
+//
+//  Created by Aige on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct BudgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/ContentView.swift
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/ContentView.swift
@@ -8,66 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isPresented: Bool = false
-    @EnvironmentObject private var model: CoffeeModel
-    func populateOrders() async {
-        do {
-            try await model.populateOrders()
-        } catch {
-            print(error)
-        }
-    }
-
-    private func deleteOrder(_ indexSet: IndexSet) {
-        indexSet.forEach { index in
-            let order = model.orders[index]
-            guard let orderId = order.id else { return }
-            Task {
-                do {
-                    try await model.deletedOrder(orderId)
-                } catch {
-                    print(error)
-                }
-            }
-        }
-    }
-
     var body: some View {
-        NavigationStack {
-            VStack {
-                if model.orders.isEmpty {
-                    Text("No orders available!").accessibilityIdentifier("noOrdersTezt")
-                } else {
-                    List {
-                        ForEach(model.orders) { order in
-                            NavigationLink(value: order.id) {
-                                OrderCellView(order: order)
-                            }
-                        }.onDelete(perform: deleteOrder)
-                    }
-                }
-            }.navigationDestination(for: Int.self, destination: { orderId in
-                OrderDetailView(orderId: orderId)
-            })
-            .task {
-                await populateOrders()
-            }
-            .sheet(isPresented: $isPresented, content: {
-                AddCoffeeView()
-            }).toolbar(content: {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add New Order") {
-                        isPresented = true
-                    }.accessibilityIdentifier("addNewOrderButton")
-                }
-            })
-        }
+        Text("")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        var config = Configuration()
-        ContentView().environmentObject(CoffeeModel(webservice: WebService(baseURL: config.environment.baseURL)))
+        ContentView()
     }
 }
