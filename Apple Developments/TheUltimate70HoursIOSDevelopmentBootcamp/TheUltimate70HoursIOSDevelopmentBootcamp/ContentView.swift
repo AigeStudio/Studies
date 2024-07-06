6c6a08982abd105ed5e3251eba1d63a1f8111247
Aige <aigestudio@hotmail.com>
2024-07-06 22:48:06 +0800

    Finished session 11-5

--- Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/ContentView.swift
+++ Apple Developments/TheUltimate70HoursIOSDevelopmentBootcamp/TheUltimate70HoursIOSDevelopmentBootcamp/ContentView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: []) private var budgetCategoryResults: FetchedResults<BudgetCategory>
+    @State private var isPresented: Bool = false
+
     var body: some View {
-        Text("")
+        NavigationStack {
+            VStack {
+                List(budgetCategoryResults) { budgetCategory in
+                    Text(budgetCategory.title ?? "")
+                }
+            }
+            .sheet(isPresented: $isPresented, content: {
+                AddBudgetCategoryView()
+            })
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add Category") {
+                        isPresented = true
+                    }
+                }
+            }).padding()
+        }
     }
 }
 
