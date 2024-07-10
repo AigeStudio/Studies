4ea59daff63b9b86f3a2511c95634a3a9308eac9
Aige <aigestudio@hotmail.com>
2024-07-10 15:25:45 +0800

    Begin of course by Stanford CS193p 2013

---
+++ Apple Developments/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by AigeStudio on 10/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .foregroundColor(Color.orange)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Apple Developments/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by AigeStudio on 10/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
