131321a20add2e741231b9da6eb978f899e1f53c
Aige <aigestudio@hotmail.com>
2024-07-18 07:09:01 +0800

    New project Emoji Art for Lecture 10

---
+++ Apple Developments/Emoji Art/Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by AigeStudio on 22/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
